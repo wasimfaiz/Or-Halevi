f6885fec8d0415f4271e3fd350143ab6a7298917
Wasim Faiz <thewasimfaiz@gmail.com>
2022-04-08 00:33:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+var tl = gsap.timeline();
+
+tl.
+from("#nav", {
+    duration: 1,
+    y: 20,
+    opacity: 0,
+    ease: "Expo.easeInOut"
+})
+
+// .from("#txt", {
+//     duration: 1,
+//     opacity: 0,
+//     ease: "Expo.easeInOut"
+// })
+
+.from(".one", {
+    duration: 0.5,
+    opacity: 0,
+    ease: "Expo.easeInOut"
+})
+
+.from(".two", {
+    duration: 0.5,
+    opacity: 0,
+    ease: "Expo.easeInOut"
+})
+
+.from(".three", {
+    duration: 0.5,
+    opacity: 0,
+    ease: "Expo.easeInOut"
+})
+
+.from(".four", {
+    duration: 0.5,
+    opacity: 0,
+    ease: "Expo.easeInOut"
+})
+
+.from(".bottxt", {
+    duration: 0.5,
+    opacity: 0,
+    ease: "Expo.easeInOut"
+})
+
+.from(".svg", {
+    duration: 0.5,
+    opacity: 0,
+    ease: "Expo.easeInOut"
+})
+
+.from(".inner", {
+    duration: 0.5,
+    opacity: 0,
+    ease: "Expo.easeInOut"
+})
+
+.from(".photu", {
+    duration: 1.5,
+    y: 100,
+    opacity: 0,
+    ease: "Expo.easeInOut"
+})
+
